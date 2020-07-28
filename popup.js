061b77d0ae96324970bf0a89fda253a7816b0ab1
Stian Remvik <stirem@gmail.com>
2020-07-28 10:50:15 +0200

    Responsive changes

--- popup.js
+++ popup.js
@@ -4,6 +4,7 @@
 document.addEventListener( 'mousedown', function ( event ) {
   if ( ! document.querySelector( '.popup-goto-link' ).contains( event.target ) ) {
     document.querySelector( '.popup-box' ).style.display = "none";
+    document.querySelector( '.popup-box-arrow' ).style.display = "none";
   }
 });
 
@@ -16,11 +17,13 @@ var popup = (function() {
   var string_goto_link_alger = "https://no.wikipedia.org/wiki/Bl%C3%A5alger";
   var string_planter = "Grønne planter er en stor plantegruppe som omfatter både encellede og flercellede alger og landplanter. Felles for disse er grønnfargen, som kommer av at kloroplastene inneholder klorofylltypene a og b. Et ytterligere fellestrekk (homologi), som oppstod i stamarten til de grønne planter, er celleveggen. ";
   var string_goto_link_planter = "https://no.wikipedia.org/wiki/Gr%C3%B8nne_planter";
-
+  var mouseX;
+  var mouseY;
 
   function showBox( clickedElementID, event ) {
 
     document.querySelector( '.popup-box' ).style.display = "block";
+    document.querySelector( '.popup-box-arrow' ).style.display = "block";
 
     if ( clickedElementID == 1 ) {
       document.querySelector( '.popup-text' ).innerHTML = string_alger;
@@ -32,45 +35,64 @@ var popup = (function() {
       document.querySelector( '.popup-goto-link' ).href = string_goto_link_planter;
     }
 
+    mouseX = event.x;
+    mouseY = event.y;
+
     // Wait for image to load before resizing and moving box
-    document.querySelector( '.popup-image' ).onload = function() { rescaleAndMoveBox( event ) };
+    document.querySelector( '.popup-image' ).onload = function() { rescaleAndMoveBox( mouseX, mouseY ) };
 
   }
 
-
-  function rescaleAndMoveBox( event ) {
+  function rescaleAndMoveBox( mouseX, mouseY ) {
 
     var popup_box_rect = document.querySelector( '.popup-box' ).getBoundingClientRect();
     var popup_box_arrow_rect = document.querySelector( '.popup-box-arrow' ).getBoundingClientRect();
-    var mouseX = event.x;
-    var mouseY = event.y;
 
     if ( mouseY > window.innerHeight/2 ) {
+
+      // ARROW
       document.querySelector( '.popup-box-arrow' ).style.top = popup_box_rect.height + 'px';
       document.querySelector( '.popup-box-arrow' ).style.borderTop = "16px solid black";
       document.querySelector( '.popup-box-arrow' ).style.borderBottom = "0";
+
+      // BOX
       document.querySelector( '.popup-box' ).style.top = mouseY + window.scrollY - popup_box_rect.height - popup_box_arrow_rect.height + 'px';
     } else {
+
+      // ARROW
       document.querySelector( '.popup-box-arrow' ).style.top = -16 + 'px';
       document.querySelector( '.popup-box-arrow' ).style.borderTop = "0";
       document.querySelector( '.popup-box-arrow' ).style.borderBottom = "16px solid black";
+
+      // BOX
       document.querySelector( '.popup-box' ).style.top = mouseY + window.scrollY + popup_box_arrow_rect.height + 'px';
+
     }
 
 
     if ( window.matchMedia("(min-width: 750px)").matches ) {
 
       if ( mouseX < window.innerWidth/2 ) {
+
+        // BOX
         document.querySelector( '.popup-box' ).style.left = mouseX - (popup_box_arrow_rect.width/2) + 'px';
+
+        // ARROW
         document.querySelector( '.popup-box-arrow' ).style.left = 0;
+
       } else {
+
+        // BOX
         document.querySelector( '.popup-box' ).style.left = mouseX - popup_box_rect.width + (popup_box_arrow_rect.width/2) + 'px';
+
+        // ARROW
         document.querySelector( '.popup-box-arrow' ).style.left = popup_box_rect.width - popup_box_arrow_rect.width + 'px';
+
       }
 
     } else {
 
-      document.querySelector( '.popup-box-arrow' ).style.left = mouseX - popup_box_arrow_rect.width - (popup_box_arrow_rect.width/2) + 'px';
+      document.querySelector( '.popup-box-arrow' ).style.left = mouseX - ( (window.innerWidth - popup_box_rect.width) / 2) - (popup_box_arrow_rect.width/4) + 'px';
 
     }
 
